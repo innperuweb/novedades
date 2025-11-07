e8b5f070b359fab7ce7e661cfe8cee7b10868b27
INNPERU WEB <innperuweb@gmail.com>
2025-11-06 22:10:13 -0500

    Eliminar archivos de base de datos obsoletos

--- database/migrations/2025_06_create_producto_categorias_web.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS producto_categorias_web (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    producto_id INT NOT NULL,
-    seccion ENUM('tienda', 'novedades', 'ofertas', 'populares', 'por_mayor') NOT NULL,
-    FOREIGN KEY (producto_id) REFERENCES productos(id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
